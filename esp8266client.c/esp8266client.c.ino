5326474baa6a146b498b47119432810b3b735299
Daniela Simões <danielasimoes@ua.pt>
2017-02-25 23:23:21 +0000

    At each iteration: scan + send.

--- esp8266client.c/esp8266client.c.ino
+++ esp8266client.c/esp8266client.c.ino
@@ -6,14 +6,17 @@ const char* password = "raspberry";
 const char* host = "192.168.0.1";
 
 void setup() {
-  Serial.begin(115200);
+  Serial.begin(115200);  
 
   WiFi.mode(WIFI_STA);
+}
+
+void loop() {
+
   WiFi.disconnect();
-  delay(100);
 
   Serial.println("scan start");
-
+  
   // WiFi.scanNetworks will return the number of networks found
   int n = WiFi.scanNetworks();
   Serial.println("scan done");
@@ -24,9 +27,6 @@ void setup() {
     Serial.println(" networks found");
     for (int i = 0; i < n; ++i)
     {
-      // Print SSID and RSSI for each network found
-      Serial.print(i + 1);
-      Serial.print(": ");
       if (WiFi.SSID(i) == ssid){
          // We start by connecting to a WiFi network
           Serial.println();
@@ -35,50 +35,38 @@ void setup() {
           Serial.println(ssid);
         
           WiFi.begin(ssid, password);
-          break;
+          
+          while (WiFi.status() != WL_CONNECTED) {
+            delay(50);
+            Serial.print(".");
+          }
+        
+          Serial.println("");
+          Serial.println("WiFi connected");
+          Serial.println("IP address: ");
+          Serial.println(WiFi.localIP()); 
+        
+          WiFiClient client;
+          const int tcpPort = 7891;
+          if (!client.connect(host, tcpPort)) {
+            Serial.println("connection failed");
+            return;
+          }
+        
+         String data = "message=ok";
+        
+         Serial.print("Sending message...: ");
+         // Send request to the server:
+         client.print(data);
+           
+         if (client.connected()) { 
+            client.stop();  // DISCONNECT FROM THE SERVER
+         }
+         Serial.println();
+         Serial.println("closing connection");
+         return;
       }
-      Serial.print(WiFi.SSID(i));
-      Serial.print(" (");
-      Serial.print(WiFi.RSSI(i));
-      Serial.print(")");
-      Serial.println((WiFi.encryptionType(i) == ENC_TYPE_NONE)?" ":"*");
-      delay(10);
+      
     }
   }
-
-
-  while (WiFi.status() != WL_CONNECTED) {
-    delay(500);
-    Serial.print(".");
-  }
-
-  Serial.println("");
-  Serial.println("WiFi connected");
-  Serial.println("IP address: ");
-  Serial.println(WiFi.localIP()); 
-
-}
-
-void loop() {
-
-  WiFiClient client;
-  const int tcpPort = 7891;
-  if (!client.connect(host, tcpPort)) {
-    Serial.println("connection failed");
-    return;
-  }
-
- String data = "message=ok";
-
-   Serial.print("Sending message...: ");
-   // Send request to the server:
-   client.print(data);
-   delay(500); // Can be changed
-   
-  if (client.connected()) { 
-    client.stop();  // DISCONNECT FROM THE SERVER
-  }
-  Serial.println();
-  Serial.println("closing connection");
-  delay(5000);
 }
